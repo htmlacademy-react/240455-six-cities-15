4b9ae0cf1a76c587fb8a364c1c3611a1cf36966d
Виктория <nikitina.post@gmail.com>
2024-03-02 22:36:22 +0300

    Заполнит компонент 404 страницы

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,11 +1,18 @@
 import Header from '../../components/header';
 
 export default function NotFound(): JSX.Element {
-  return (
-    <div className="page page--gray page--main">
-      <Header />
-      <main className="page__main page__main--index">
 
+  const style = {
+    textAlign: 'center' as const,
+
+  };
+
+  return (
+    <div>
+      <Header />
+      <main style={style}>
+        <h1>Page not found</h1>
+        <p><a href='/'>Return to Homepage</a></p>
       </main>
     </div>
   );
